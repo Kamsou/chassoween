34757c1443b359d5a88db6794c80047bd9f69a3d
sylvie <ngh.sylvie@gmail.com>
2018-10-31 11:21:12 +0100

    logoween bounce

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.css';
 import logo from '../img/logo-ween.png';
+import {Animated} from "react-animated-css";
 
 class Header extends React.Component {
     render() {
         return(
             <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+            <Animated animationIn="bounce" animationOut="bounce" > <img src={logo} className="App-logo" alt="logo" /></Animated>
             </header>
             </div>
         )
